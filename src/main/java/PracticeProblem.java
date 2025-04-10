b473776a779a4bee7dd555b6be3900ec3ded256e
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-04-10 19:16:12 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,47 @@
+import java.util.HashMap;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static int fib(int num) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		return fibHelper(num, map);
+	}
+
+	public static int fibHelper(int num, HashMap<Integer, Integer> map) {
+		if (num == 0) {
+			return 0;
+		} else if (num == 1) {
+			return 1;
+		} else if (map.containsKey(num)) {
+			return map.get(num);
+		}
+
+		int result = fibHelper(num - 1, map) + fibHelper(num - 2, map);
+		map.put(num, result);
+		return result;
+	}
+
+	public static int minCostClimbingStairs(int[] num) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		if (num.length == 1) {
+			return stairsHelper(num, 0, 0, map);
+		}
+		return Math.min(stairsHelper(num, 0, 0, map), stairsHelper(num, 1, 0, map));
+	}
+
+	public static int stairsHelper(int[] nums, int pos, int currCost, HashMap<Integer, Integer> map) {
+		if (pos >= nums.length) {
+			return currCost;
+		} else if (map.containsKey(pos)) {
+			return map.get(pos);
+		}
+
+		int result = Math.min(stairsHelper(nums, pos + 1, currCost, map), stairsHelper(nums, pos + 2, currCost, map)) + nums[pos];
+		map.put(pos, result);
+		return result;
+	}
 }
